20f714d79a985492c77850967a8b045478a94a28
th-inu <krng0111@naver.com>
2021-11-17 15:00:51 +0900

    connect react and node.js

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const express  = require('express');
-const app = express();
-const api = require('./routes/index');
-
-app.use('/api', api);
-
-const port = 3002;
-app.listen(port,()=>console.log(`Listening on port ${port}`));
