bd8bb8e520606244f721c17613d6568233ccdad6
th-inu <krng0111@naver.com>
2021-11-17 15:27:31 +0900

    connect mariaDB (to do testing)

--- myapp/server/server.js
+++ myapp/server/server.js
@@ -2,13 +2,22 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const port =process.env.PORT || 3001;
 
+// db 연결
+const conn = require('../conf/db.js');
+conn.connect();
+
+// 보안 허용
 app.use(cors());
 
-app.use(bodyParser.json());
-app.use('/api', (req, res)=> res.json({username:'bryan'}));
-
+// 서버 열기
+const port =process.env.PORT || 3001;   // port num 3001
 app.listen(port, ()=>{
     console.log(`express is running on ${port}`);
 })
+
+// localhost:3000/api
+app.use(bodyParser.json());
+app.use('/api', (req, res)=> { 
+    res.json({username:'TH'});  // TH를 react로 전송
+});
