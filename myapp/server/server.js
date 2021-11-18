7e500632e949ea3abc040921d37607ca0d798193
th-inu <krng0111@naver.com>
2021-11-18 23:41:18 +0900

    draw graph with json data with chart.js

--- myapp/server/server.js
+++ myapp/server/server.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 // db 연결
-const conn = require('../conf/db.js');
-conn.connect();
+// const conn = require('../conf/db.js');
+// conn.connect();
 
 // 보안 허용
 app.use(cors());
@@ -19,14 +19,42 @@ app.listen(port, ()=>{
 // localhost:3000/api
 app.use(bodyParser.json());
 app.use('/api', (req, res)=> {
-    var sql = 'select * from new_csi_1116 order by id desc limit 20';
-    conn.query(sql, (err, rows, fields) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('rows: ', rows);
-            console.log('fields', fields);
-            res.json({csiData: rows});  // TH를 react로 전송
-        }
-    });
+    // var sql = 'select id, section, csi_data from ploting_data order by id desc limit 5';
+    // conn.query(sql, (err, rows, fields) => {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         console.log('rows: ', rows);
+    //         console.log('fields', fields);
+
+    //         res.set({
+    //             "Content-Type"  : "text/event-stream",
+    //             "Cache-Control" : "no-cache",
+    //             Connection: "keep-alive",
+
+    //             "Access-Control-Allow-Otrigin": "*",
+    //             "Access-Control-Aloow-Headers":
+    //                 "Origin, X-Requested-With, Content-Type, Accept"
+    //         });
+
+    //         setInterval(() => {
+    //             res.write(`data: ${JSON.stringify(rows)}\n\n`);
+    //             console.log("${JSON.stringify(getData())");
+    //         }, 2000);
+            
+    //         req.on('close', (err) => {
+    //             clearInterval(eventInterval);
+    //             res.end();
+    //         });
+    //         // res.json({csiData: rows});  // TH를 react로 전송
+    //     }
+    // });
+
+    var csiData = [{id:37, section:1, csi_data:[112, 15, 0, 0, 0, 0, 8, 10, 13, 15, 16, 18, 19, 19, 18, 17, 16, 15, 13, 10, 9, 7, 9, 11, 13, 16, 18, 20, 21, 22, 22, 20, 0, 17, 15, 13, 11, 10, 9, 10, 12, 13, 15, 16, 17, 17, 16, 16, 15, 15, 13, 12, 12, 12, 13, 15, 17, 18, 18, 0, 0, 0, 0], predict:4, total_predict:4},
+    {id:38, section:2, csi_data:[18, 14, 0, 0, 0, 0, 12, 14, 16, 17, 18, 18, 19, 18, 18, 16, 17, 17, 15, 15, 15, 15, 15, 16, 16, 17, 18, 18, 21, 23, 25, 25, 0, 24, 21, 20, 18, 14, 12, 12, 13, 17, 21, 25, 27, 29, 30, 31, 30, 27, 23, 17, 14, 11, 9, 12, 16, 19, 20, 0, 0, 0, 0], predict:4, total_predict:4},
+    {id:39, section:3, csi_data:[18, 14, 0, 0, 0, 0, 8, 10, 11, 12, 14, 16, 16, 17, 16, 13, 13, 12, 12, 12, 13, 14, 17, 16, 16, 15, 13, 12, 10, 8, 8, 10, 0, 13, 13, 15, 16, 17, 17, 16, 15, 15, 14, 13, 13, 15, 15, 20, 21, 20, 23, 23, 22, 20, 19, 18, 18, 18, 18, 0, 0, 0, 0], predict:4, total_predict:4},
+    {id:40, section:1, csi_data:[112, 15, 0, 0, 0, 0, 8, 10, 13, 15, 16, 18, 19, 19, 18, 17, 16, 15, 13, 10, 9, 7, 9, 11, 13, 16, 18, 20, 21, 22, 22, 20, 0, 17, 15, 13, 11, 10, 9, 10, 12, 13, 15, 16, 17, 17, 16, 16, 15, 15, 13, 12, 12, 12, 13, 15, 17, 18, 18, 0, 0, 0, 0], predict:4, total_predict:4},
+    {id:41, section:2, csi_data:[18, 14, 0, 0, 0, 0, 12, 14, 16, 17, 18, 18, 19, 18, 18, 16, 17, 17, 15, 15, 15, 15, 15, 16, 16, 17, 18, 18, 21, 23, 25, 25, 0, 24, 21, 20, 18, 14, 12, 12, 13, 17, 21, 25, 27, 29, 30, 31, 30, 27, 23, 17, 14, 11, 9, 12, 16, 19, 20, 0, 0, 0, 0], predict:4, total_predict:4},
+    {id:42, section:3, csi_data:[18, 14, 0, 0, 0, 0, 8, 10, 11, 12, 14, 16, 16, 17, 16, 13, 13, 12, 12, 12, 13, 14, 17, 16, 16, 15, 13, 12, 10, 8, 8, 10, 0, 13, 13, 15, 16, 17, 17, 16, 15, 15, 14, 13, 13, 15, 15, 20, 21, 20, 23, 23, 22, 20, 19, 18, 18, 18, 18, 0, 0, 0, 0], predict:4, total_predict:4}]
+    res.json({csiData});
 });
--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import Chart from './component/showGraph'
+import { Line } from 'react-chartjs-2';
+
+const options = {
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  }
+};
 
 class App extends React.Component {
 
+  // state에 사용할 변수 초기화
   constructor(props) {
       super(props);
       this.state = {
@@ -12,6 +20,7 @@ class App extends React.Component {
       };
   }
 
+  // json 파일로 받아온 데이터 parsing
   componentDidMount() {
     fetch('http://localhost:3001/api')
         .then(res=>res.json())
@@ -20,10 +29,38 @@ class App extends React.Component {
 
   render() {
     const {csiData} = this.state;
+
+    // x축 label 생성
+    const label = []
+    for (var i = 0; i < 20; i++) {
+      label[i] = ''+i
+    }
+
+    // csi 데이터만 뽑아내기
+    var csi_data = [];
+    var i = 0;
+    csiData.forEach( csiData => { 
+      csi_data[i++] = csiData.csi_data;
+    })
+
+    // dataSet 만들기
+    const dataSet = [];
+    for (var j = 0; j < 6; j++) {
+      dataSet[j] = {
+        label: +j+' of csi_data',
+        data: csi_data[j],
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+      }
+    }
+
+    const data = {labels: label, datasets: dataSet};
+    console.log(data);
+
     return (
       <div className="App">
-        <Chart/>
-        {console.log(csiData)}
+        <Line data={data} options={options} />
       </div>
     );
   }
