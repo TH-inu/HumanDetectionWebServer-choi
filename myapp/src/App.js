8bbfabe1ba2068fa3d2b3e3bbc668ea8f10bee31
th-inu <krng0111@naver.com>
2021-11-22 15:52:03 +0900

    test

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,6 +2,42 @@ import React from 'react';
 import './App.css';
 import { Line } from 'react-chartjs-2';
 
+function data_processing(csiData) {
+  for (var i = 0; i < 64; i++) {
+    label[i] = ''+i
+  }
+
+  // csi 데이터만 뽑아내기
+  var csi_data = [];
+  var i = 0;
+  csiData.forEach(csiData => {
+    // console.log(csiData);
+    csi_data[i++] = csiData.csi_data.split(', ').map(item => {
+      return parseInt(item, 10);
+    }); // string to int array
+  });
+  // csiData.forEach( csiData => { 
+  //   csi_data[i++] = csiData.csi_data.split(', ').map(item => {
+  //     return parseInt(item, 10);
+  //   }); // string to int array
+  // });
+
+  // dataSet 만들기
+  for (var j = 0; j < 20; j++) {
+    dataSet[j] = {
+      label: +j+' of csi_data',
+      data: csi_data[j],
+      fill: false,
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgba(255, 99, 132, 0.2)',
+    }
+  }
+  let return_data = {labels: label, datasets: dataSet};
+  return return_data
+}
+
+const stream_url = 'http://localhost:5001/api'
+
 const options = {
   scales: {
     y: {
@@ -9,16 +45,25 @@ const options = {
     }
   }
 };
+const label = []
+const dataSet = [];
+let data = {labels: label, datasets: dataSet};
+let lineChart;
 
-const stream_url = 'http://localhost:3001/api'
 var es = new EventSource(stream_url);
 var csiData = []
 es.onmessage = function (event) {
   csiData = event.data;
-  // json 파일로 받아온 데이터 parsing
-  // csiData = event.data;  
-  // 이벤트 설정이안된 기본 데이터 처리
-};
+  data = data_processing(JSON.parse(event.data));
+this.setState(this.onChange(data));
+// var es = new EventSource(stream_url);
+// var csiData = []
+// es.onmessage = function (event) {
+//   csiData = event.data;
+//   data = data_processing(JSON.parse(event.data));
+//   this.setState(this.onChange(data));
+//   // console.log(data);
+// };
 es.addEventListener('myevent', function(e) {
     // 'myevent' 이벤트의 데이터 처리
     // console.log(e);
@@ -27,59 +72,40 @@ es.addEventListener('myevent', function(e) {
 }, false);
 
 class App extends React.Component {
-
+  // console.log(data);
+};
   // state에 사용할 변수 초기화
-  constructor(props) {
-      super(props);
-      this.state = {
-          csiData:[]
-      };
-  }
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //         csiData:[]
+  //     };
+  // }
 
   // json 파일로 받아온 데이터 parsing
-  componentDidMount() {
-    fetch(stream_url)
-        .then(res=>res.json())
-        .then(data=>this.setState({csiData : data.csiData}));
+  // componentDidMount() {
+  //   fetch(stream_url)
+  //       .then(res=>res.json())
+  //       .then(data=>this.setState({csiData : data.csiData}));
+  // }
+  conponentDidMount() {
+    console.log(this.chartReference);
+    lineChart = this.reference.charInstance;
+    console.log(lineChart);
   }
 
   render() {
-    const {csiData} = this.state;
+    // const {csiData} = this.state;
     // console.log(csiData);
     // x축 label 생성
-    const label = []
-    for (var i = 0; i < 64; i++) {
-      label[i] = ''+i
-    }
-
-    // csi 데이터만 뽑아내기
-    var csi_data = [];
-    var i = 0;
-    csiData.forEach( csiData => { 
-      csi_data[i++] = csiData.csi_data.split(', ').map(item => {
-        return parseInt(item, 10);
-      }); // string to int array
-    })
-
-    // dataSet 만들기
-    const dataSet = [];
-    for (var j = 0; j < 20; j++) {
-      dataSet[j] = {
-        label: +j+' of csi_data',
-        data: csi_data[j],
-        fill: false,
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgba(255, 99, 132, 0.2)',
-      }
-    }
-
-    const data = {labels: label, datasets: dataSet};
-    console.log(csiData);
-    console.log(data);
 
     return (
       <div className="App">
-        <Line data={data} options={options} />
+        {/* {draw_graph()} */}
+        {/* <Line data={data} options={options} /> */}
+        <Line  
+          data={this.data}
+          ref={(reference) => this.chartReference = reference} />
       </div>
     );
   }
